2e4302f154a37b6dccf7943f674f58a662b78767
Lubomir Yordanov <106163099+LubomirYordanov17@users.noreply.github.com>
2023-07-31 17:12:35 +0300

    Add files via upload

---
+++ Sneaker Closet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sneaker_Closet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
